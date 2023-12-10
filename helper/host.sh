5bb161cacd8c629bce1b9c709919979094d3d3f8
Rayann <71392060+Ayato-san@users.noreply.github.com>
2023-12-10 20:25:12 +0100

    Create host.sh

---
+++ helper/host.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+if [ "$EUID" -ne 0]; then
+  echo "Please run as root"
+  exit
+fi
+
+old=$(hostname)
+
+echo "What is the new NAME?"
+read new
+echo "$new" > /etc/hostname
+sed -i -e "s/$old/$new/g" /etc/hosts
+echo "The old hostname was $old the new is $new"
+echo "The computer will reboot in 3 seconds..."
+
+sleep 2
+reboot

c3d33b5af72690d9565cf48944946cab9a942f00
Rayann <71392060+Ayato-san@users.noreply.github.com>
2023-12-10 21:42:52 +0100

    Update redmine-part-3.sh

--- install/redmine-part-3.sh
+++ install/redmine-part-3.sh
@@ -7,4 +7,5 @@ cd /opt/redmine
 gem install bundler
 
 echo "Switching to redmine user account"
+echo "run now : bash -c \"$(wget -qLO - https://git.ayato-san.fr/shell-scripts/main/install/redmine-part-4.sh)\""
 su - redmine

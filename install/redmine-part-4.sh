5e72e0a4dbf2d5b32a0595e56dfc4b09bb508465
Rayann <71392060+Ayato-san@users.noreply.github.com>
2023-12-10 20:49:36 +0100

    Create redmine-part-4.sh

---
+++ install/redmine-part-4.sh
@@ -0,0 +1,42 @@
+bundle install --without development test --path vendor/bundle
+bundle exec rake generate_secret_token
+RAILS_ENV=production bundle exec rake db:migrate
+RAILS_ENV=production REDMINE_LANG=fr bundle exec rake redmine:load_default_data
+for i in tmp tmp/pdf public/plugin_assets; do [ -d $i ] || mkdir -p $i; done
+chown -R redmine:redmine files log tmp public/plugin_assets
+chmod -R 755 /opt/redmine
+exit
+echo "Ruby Configuration complete"
+exit
+
+echo ""
+echo "Configuring Apache..."
+echo "Writing redmine.conf"
+echo "<VirtualHost *:80>" > /etc/apache2/sites-available/redmine.conf
+echo "	ServerName localhost" >> /etc/apache2/sites-available/redmine.conf
+echo "	RailsEnv production" >> /etc/apache2/sites-available/redmine.conf
+echo "	DocumentRoot /opt/redmine/public" >> /etc/apache2/sites-available/redmine.conf
+echo "" >> /etc/apache2/sites-available/redmine.conf
+echo "	<Directory \"/opt/redmine/public\">" >> /etc/apache2/sites-available/redmine.conf
+echo "		Allow from all" >> /etc/apache2/sites-available/redmine.conf
+echo "		Require all granted" >> /etc/apache2/sites-available/redmine.conf
+echo "	</Directory>" >> /etc/apache2/sites-available/redmine.conf
+echo "" >> /etc/apache2/sites-available/redmine.conf
+echo "	 ErrorLog \${APACHE_LOG_DIR}/redmine_error.log" >> /etc/apache2/sites-available/redmine.conf
+echo "	CustomLog \${APACHE_LOG_DIR}/redmine_access.log combined" >> /etc/apache2/sites-available/redmine.conf
+echo "</VirtualHost>" >> /etc/apache2/sites-available/redmine.conf
+echo "Apache Configuration Complete"
+
+clear
+echo "--------------------------------------"
+echo "  Setting up Redmine"
+echo "--------------------------------------"
+echo "  IP:           $IP"
+echo "  Domain name:  $DOMAIN_NAME"
+echo "--------------------------------------"
+echo "Before launching you can verify :"
+echo "  - /opt/redmine/config/database.yml"
+echo "  - /etc/apache2/sites-available/redmine.conf"
+echo ""
+echo "run : a2ensite redmine"
+echo "run : systemctl reload apache2"
